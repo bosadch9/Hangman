115fe4b9ced5db14a278b1cdb6644879a157fe08
Emma Weiner 2019 <eweiner9@3012-l29545-s.local>
2019-05-10 09:31:41 -0500

    This code lets you click a button to generate a random word

--- Hangman/ViewController.swift
+++ Hangman/ViewController.swift
@@ -1,21 +1,38 @@
 //
 //  ViewController.swift
 //  Hangman
-//burger
-//Emma was here
 //  Created by Bohdan Osadchyy 2019 on 5/7/19.
 //  Copyright © 2019 Bohdan Osadchyy 2019. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    //This generates a random word
+    @IBAction func buttonClicked(_ sender: UIButton)
+    {
+        if let wordsFilePath = Bundle.main.path(forResource: "english3.txt", ofType: nil) {
+            do {
+                let wordsString = try String(contentsOfFile: wordsFilePath)
+                
+                let wordLines = wordsString.components(separatedBy: .newlines)
+                
+                let randomWord = wordLines[numericCast(arc4random_uniform(numericCast(wordLines.count)))]
+                
+                print(randomWord)
+                
+            } catch {
+                print("Error: \(error)")
+            }
+        }
+    }
 
 }
 
