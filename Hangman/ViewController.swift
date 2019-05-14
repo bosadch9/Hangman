0e45b880eaffc40a193dd952adfcdd1befa358cb
Emma Weiner 2019 <eweiner9@3012-l29545-s.local>
2019-05-14 10:05:19 -0500

    Random word and letter spaces

--- Hangman/ViewController.swift
+++ Hangman/ViewController.swift
@@ -9,30 +9,50 @@ import UIKit
 
 class ViewController: UIViewController
 {
-    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var viewHolder: UIView!
     var currentWord: String = ""
     var letters: [String] = Array()
     var labels: [UILabel] = Array()
+    @IBOutlet weak var guessButton: UIButton!
+    @IBOutlet weak var currentGuessTextfield: UITextField!
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
     }
 
+    //This starts the game. A random word is selected and blank spaces are displayed on the screen
+    func startGame()
+    {
+        letters.removeAll()
+        labels.removeAll()
+        getWord()
+        for letter in currentWord
+        {
+            letters.append("\(letter)")
+            var label = UILabel()
+            label.text = "_"
+            labels.append(label)
+        }
+        let stackView1 = UIStackView(arrangedSubviews: labels)
+        stackView1.axis = .horizontal
+        stackView1.distribution = .fillEqually
+        stackView1.alignment = .fill
+        stackView1.spacing = 15
+        stackView1.translatesAutoresizingMaskIntoConstraints = false
+        viewHolder.addSubview(stackView1)
+    }
+    
     //This generates a random word
-    @IBAction func buttonClicked(_ sender: UIButton)
+    func getWord()
     {
         if let wordsFilePath = Bundle.main.path(forResource: "english3.txt", ofType: nil) {
             do {
                 let wordsString = try String(contentsOfFile: wordsFilePath)
-                
                 let wordLines = wordsString.components(separatedBy: .newlines)
+                currentWord = wordLines[numericCast(arc4random_uniform(numericCast(wordLines.count)))]
                 
-                let randomWord = wordLines[numericCast(arc4random_uniform(numericCast(wordLines.count)))]
-                
-                print(randomWord)
-                currentWord = randomWord
-                test()
+                print(currentWord)
             }
             catch
             {
@@ -41,33 +61,27 @@ class ViewController: UIViewController
         }
     }
     
-    func test()
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let currentText = textField.text ?? ""
+        guard let stringRange = Range(range, in: currentText) else { return false }
+        
+        let updatedText = currentText.replacingCharacters(in: stringRange, with: string)
+        
+        return updatedText.count <= 1
+    }
+    
+    @IBAction func guessButtonPressed(_ sender: UIButton)
     {
-        for letter in currentWord
+        
+        
+        //if let guessedChar = Character(currentGuessTextfield.text!)
+        for letter in letters
         {
-            letters.append("\(letter)")
-            var label = UILabel()
-            label.text = "\(letter)"
-            labels.append(label)
+                
         }
         
-        
-        let stackView1 = UIStackView(arrangedSubviews: labels)
-        //stackView1.frame.width = 200
-        //stackView1.frame.height = 100
-        //stackView1.frame.minX = 0
-        stackView1.axis = .horizontal
-        stackView1.distribution = .fillEqually
-        stackView1.alignment = .fill
-        stackView1.spacing = 5
-        stackView1.translatesAutoresizingMaskIntoConstraints = false
-        //let horizontalConstraint = stackView1.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-        
-        //let verticalConstraint = stackView1.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        //stackView1.addConstraint(verticalConstraint)
-        //NSLayoutConstraint.activate([verticalConstraint])
-        view.addSubview(stackView1)
     }
+    
 
 }
 
